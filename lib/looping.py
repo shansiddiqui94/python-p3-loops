608adc3914962d8ee7144317652dcccc77b962fa
Shan Siddiqui <49912126+shansiddiqui94@users.noreply.github.com>
2024-03-15 18:26:57 -0400

    loops

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,31 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        print(f"{i}")
+        i -= 1
+        print("Happy New Year!")
+    
+def square_integers(int_list): 
+    squared_list = []  # Initialize an empty list to store squared elements
+    for num in int_list:
+        squared_list.append(num ** 2)  # Square each integer and append to the squared_list
+    return squared_list
 
-def square_integers(int_list):
-    # code goes here!
-    pass
+   
 
 def fizzbuzz():
-    # code goes here!
-    pass
+     for num in range(1, 101):  # Iterate over numbers from 1 to 100
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
+
+   
+   
+ 
